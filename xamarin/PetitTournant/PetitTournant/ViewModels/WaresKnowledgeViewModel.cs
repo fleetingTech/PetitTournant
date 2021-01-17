87b9dd2360f38e2c1de099698ca4460dd1b7e6fe
fleetingTech <git@fleeting.technology>
2021-01-17 18:18:02 +0100

    cleanup of project move

---
+++ xamarin/PetitTournant/PetitTournant/ViewModels/WaresKnowledgeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetitTournant.ViewModels
+{
+    class WaresKnowledgeViewModel : KnowledgeViewModel
+    {
+        public string PageTitle => Localisation.StringLocalisation.MerchandiseKnowledgeName;
+    }
+}
---
+++ xamarin/PetitTournant/PetitTournant/Views/KnowledgePage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PetitTournant.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class KnowledgePage : ContentPage
+	{
+		private KnowledgePage ()
+		{
+			InitializeComponent ();
+		}
+        private void SetBindingContext(ViewModels.KnowledgeViewModel model)
+        {
+            this.BindingContext = model;
+        }
+        public static KnowledgePage GetKitchenKnowledgePage()
+        {
+            KnowledgePage page = new KnowledgePage();
+            page.SetBindingContext(new ViewModels.KitchenKnowledgeViewModel());
+            return page;
+        }
+        public static KnowledgePage GetWaresKnowledgePage()
+        {
+            KnowledgePage page = new KnowledgePage();
+            page.SetBindingContext(new ViewModels.WaresKnowledgeViewModel());
+            return page;
+        }
+    }
+}
